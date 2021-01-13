d1b7c027bfb77ae39a94417619ec35786a1d0e2c
k1te <kmiller343@gmail.com>
2021-01-13 01:26:54 -0600

    created basic results container component

---
+++ client/src/components/ResultsContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ResultsContainer = (props) => {
+    return (
+        <div className="container">
+            <div className="row">
+                <h5>Results</h5>
+            </div>
+        </div>
+    )
+}
+
+export default ResultsContainer;
