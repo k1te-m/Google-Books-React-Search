fddd0c4fa5146838da68421264ea2d86aa9e7939
k1te <kmiller343@gmail.com>
2021-01-13 03:59:51 -0600

    basic results container with title/authors

--- client/src/components/ResultsContainer.js
+++ client/src/components/ResultsContainer.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React from "react";
 
 const ResultsContainer = (props) => {
-    return (
-        <div className="container">
-            <div className="row">
-                <h5>Results</h5>
+  const results = props.results.searchedBooks;
+  console.log(results);
+  return (
+    <div className="container">
+      <div className="row">
+        <h5>Results</h5>
+      </div>
+      <div className="row">
+        {results.map((book) =>
+            <div className="container">
+                <div className="row">
+                    <h5>{book.volumeInfo.title}</h5>
+                    <span>Written by:</span>
+                    {book.volumeInfo.authors.map((author) => 
+                        <span>{author}</span>
+                    )}
+                </div>
             </div>
-        </div>
-    )
-}
+        )}
+      </div>
+    </div>
+  );
+};
+
 
 export default ResultsContainer;
