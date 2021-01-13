2408a6a9f50f3c12ff4e610d3c62fab865ec1525
k1te <kmiller343@gmail.com>
2021-01-12 19:27:36 -0600

    updated server file for mongoose/middleware

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const path = require("path");
+const mongoose = require('mongoose');
+const routes = require('./routes')
+
+//Express
+const app = express();
+
+// Variable Port
+const PORT = process.env.PORT || 3001;
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", 
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
+
+// API Routes
+app.use(routes);
+
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, function() {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
