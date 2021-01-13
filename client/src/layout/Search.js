5d9cab9917f0a9110f2f8c4baf7eea9553d253e0
k1te <kmiller343@gmail.com>
2021-01-13 01:27:03 -0600

    imported results container

--- client/src/layout/Search.js
+++ client/src/layout/Search.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import BookSearch from '../components/BookSearch'
+import ResultsContainer from '../components/ResultsContainer';
 
 const Search = () => {
     return (
         <div className="container">
-            <BookSearch />
+            <div className="row">
+                <BookSearch />
+            </div>
+            <div className="row">
+               <ResultsContainer />
+            </div>
+            
         </div>
     )
 }
