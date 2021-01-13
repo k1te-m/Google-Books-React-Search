7697cc16bda740db990a79c85f8e23ddfff57ddb
k1te <kmiller343@gmail.com>
2021-01-13 04:00:19 -0600

    cleaned up Search layout

--- client/src/layout/Search.js
+++ client/src/layout/Search.js
@@ -4,15 +4,7 @@ import ResultsContainer from '../components/ResultsContainer';
 
 const Search = () => {
     return (
-        <div className="container">
-            <div className="row">
-                <BookSearch />
-            </div>
-            <div className="row">
-               <ResultsContainer />
-            </div>
-            
-        </div>
+        <BookSearch />
     )
 }
 
