fa36ba01a568540fe3e904ef832c79b0d2c69a52
k1te <kmiller343@gmail.com>
2021-01-13 15:04:52 -0600

    created saveBook function

--- client/src/components/ResultsContainer.js
+++ client/src/components/ResultsContainer.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import API from "../utils/API";
 
 const ResultsContainerDiv = styled.div`
   border: 1px solid black;
@@ -18,49 +19,79 @@ const BookCard = styled.div`
 `;
 
 const ResultsContainer = (props) => {
+  const [bookInfo, setBookInfo] = useState({
+    title: "",
+    authors: [""],
+    description: "",
+    image: "",
+    link: "",
+  });
+
+  const saveBook = (book) => {
+    setBookInfo({
+      ...bookInfo,
+      title: book.volumeInfo.title,
+      authors: book.volumeInfo.authors,
+      description: book.volumeInfo.description,
+      image: book.volumeInfo.imageLinks.thumbnail,
+      link: book.volumeInfo.previewLink,
+    });
+    API.saveBook({
+      ...bookInfo,
+      title: book.volumeInfo.title,
+      authors: book.volumeInfo.authors,
+      description: book.volumeInfo.description,
+      image: book.volumeInfo.imageLinks.thumbnail,
+      link: book.volumeInfo.previewLink,
+    })
+      .then((res) => {
+        if (res.status === 200) {
+          alert("Book Saved");
+        }
+      })
+      .catch((error) => console.error(error));
+  };
+
   const results = props.results.searchedBooks;
-  console.log(results);
   return (
     <ResultsContainerDiv className="container">
       <div className="row">
         <h5>Results</h5>
       </div>
       <div className="row">
-        {results.map(
-          (book) => (
-            <BookCard className="card">
-              <div className="row">
-                <div className="col">
-                  <h5>{book.volumeInfo.title}</h5>
-                  <span>Written by: </span>
-                  {book.volumeInfo.authors.map((author) => (
-                    <span>{author}</span>
-                  ))}
-                </div>
-                <div className="col d-flex justify-content-end">
-                  <a
-                    href={book.volumeInfo.previewLink}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <button>View</button>
-                  </a>
-                  <a>
-                    <button>Save</button>
-                  </a>
-                </div>
+        {results.map((book) => (
+          <BookCard className="card">
+            <div className="row">
+              <div className="col">
+                <h5>{book.volumeInfo.title}</h5>
+                <span>Written by: </span>
+                {book.volumeInfo.authors.map((author) => (
+                  <span>{author}</span>
+                ))}
               </div>
-              <div className="row">
-                <div className="col-3 d-flex justify-content-center">
-                  <BookImage src={book.volumeInfo.imageLinks.thumbnail} />
-                </div>
-                <div className="col-9">
-                  <p>{book.volumeInfo.description}</p>
-                </div>
+              <div className="col d-flex justify-content-end">
+                <a
+                  href={book.volumeInfo.previewLink}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button>View</button>
+                </a>
+                <a>
+                  <button onClick={() => saveBook(book)}>Save</button>
+                </a>
               </div>
-            </BookCard>
-          )
-        )}
+            </div>
+            <div className="row">
+              <div className="col-3 d-flex justify-content-center">
+                <BookImage src={book.volumeInfo.imageLinks.thumbnail} />
+              </div>
+              <div className="col-9">
+                <p>{book.volumeInfo.description}</p>
+              </div>
+            </div>
+          </BookCard>
+        ))}
       </div>
     </ResultsContainerDiv>
   );
