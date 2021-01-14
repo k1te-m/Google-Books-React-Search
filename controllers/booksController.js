e1990cd5952b90ec2dc1f986959df6201f1d6bc8
k1te <kmiller343@gmail.com>
2021-01-14 02:24:59 -0600

    booksController for saving and retrieving books

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -1,15 +1,18 @@
-const db = require('../models');
-
+const db = require("../models");
 
 module.exports = {
-    saveBook: (req, res) => {
-        console.log(req.body);
-        db.Book
-          .create(req.body)
-          .then((book) => {
-            res.json(book);
-            console.log('Book Saved!');
-          })
-          .catch((error) => res.status(422).json(error))
-    }    
-}
+  getBooks: (req, res) => {
+    db.Book.find({})
+      .then((books) => res.json(books))
+      .catch((error) => res.status(422).json(error));
+  },
+  saveBook: (req, res) => {
+    console.log(req.body);
+    db.Book.create(req.body)
+      .then((book) => {
+        res.json(book);
+        console.log("Book Saved!");
+      })
+      .catch((error) => res.status(422).json(error));
+  },
+};
