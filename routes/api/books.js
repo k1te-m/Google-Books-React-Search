c1e1dedde20562138a029e576e04b09dfc7836fb
k1te <kmiller343@gmail.com>
2021-01-14 02:30:10 -0600

    added get request for /api/books

--- routes/api/books.js
+++ routes/api/books.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const booksController = require("../../controllers/booksController");
 
 router.route("/").post(booksController.saveBook);
+router.route("/").get(booksController.getBooks);
 
 module.exports = router;
