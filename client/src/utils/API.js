c592a1dbcb06c27e0953d23eef464a9ee7987e85
k1te <kmiller343@gmail.com>
2021-01-14 02:29:55 -0600

    front end routes

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
 export default {
+    getBooks: () => {
+        return axios.get("/api/books")
+    },
     saveBook: (data) => {
         return axios.post("/api/books", data)
     }
