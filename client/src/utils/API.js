35dc6805c155023230d542a2cc2a59c22ccba278
k1te <kmiller343@gmail.com>
2021-01-14 15:21:44 -0600

    added deleteBook function to utils

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,9 +2,12 @@ import axios from 'axios';
 
 export default {
     getBooks: () => {
-        return axios.get("/api/books")
+        return axios.get("/api/books");
     },
     saveBook: (data) => {
-        return axios.post("/api/books", data)
+        return axios.post("/api/books", data);
+    },
+    deleteBook: (id) => {
+        return axios.delete('/api/books/' + id);
     }
 }
